52f45c9c7fa04a0ba5895c25e83be46bfe2b228c
Adam Jonas <adamjonas25@gmail.com>
2021-06-16 22:53:16 +0100

    Define constant for default capacity and full() boolean method

--- src/main/java/Airport.java
+++ src/main/java/Airport.java
@@ -3,6 +3,7 @@ import java.util.List;
 
 class Airport {
     private final List<Plane> hangar;
+    private final int DEFAULT_CAPACITY = 50;
 
     Airport() {
         this.hangar = new ArrayList<>();
@@ -13,7 +14,11 @@ class Airport {
     }
 
     public void land(Plane plane) throws CapacityException {
-        if (hangar.size() >= 50) throw new CapacityException();
+        if (full()) throw new CapacityException();
         hangar.add(plane);
+    }
+
+    private boolean full() {
+        return hangar.size() >= DEFAULT_CAPACITY;
     }
 }
